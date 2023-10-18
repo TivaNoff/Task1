36796b4e5c1098aea76dd3638e852cbdb38669c3
TivaN <TivaN@DESKTOP-J6PVEAM>
2023-10-18 16:20:01 +0300

    Добавьте файлы проекта.

---
+++ Task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+void printR(std::string str, int lng)
+{
+    if (lng >= 0)
+    {
+        if (lng < str.length()) {
+            std::cout << str[lng];
+        }
+        printR(str, lng - 1);
+    }
+}
+
+int main()
+{
+    std::string str = "tiger";
+    printR(str, str.length() - 1);
+    return 0;
+}
